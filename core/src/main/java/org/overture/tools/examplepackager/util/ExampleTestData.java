b1d7a49f5a2b575349917df452baeaf450f0be33
Luis Diogo Couto <ldc@eng.au.dk>
2014-07-01 18:56:06 +0200

    Add release version to example TestData.

--- core/src/main/java/org/overture/tools/examplepackager/util/ExampleTestData.java
+++ core/src/main/java/org/overture/tools/examplepackager/util/ExampleTestData.java
@@ -1,28 +1,42 @@
 package org.overture.tools.examplepackager.util;
 
 import org.overture.tools.examplepackager.Dialect;
+import org.overture.tools.examplepackager.Release;
 
-public class ExampleTestData {
+public class ExampleTestData
+{
 
 	String name;
 	Dialect dialect;
+	Release release;
 	String source;
 
-	public ExampleTestData(String name, Dialect dialect, String source) {
+	public ExampleTestData(String name, Dialect dialect, Release release,
+			String source)
+	{
 		this.name = name;
 		this.dialect = dialect;
 		this.source = source;
+		this.release = release;
 	}
 
-	public String getName() {
+	public Release getRelease()
+	{
+		return release;
+	}
+
+	public String getName()
+	{
 		return name;
 	}
 
-	public Dialect getDialect() {
+	public Dialect getDialect()
+	{
 		return dialect;
 	}
 
-	public String getSource() {
+	public String getSource()
+	{
 		return source;
 	}
 
