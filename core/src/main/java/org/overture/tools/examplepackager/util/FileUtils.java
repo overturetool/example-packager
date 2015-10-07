4e9e9224e0419dc2f370124377a93a5aa03d31b9
Luis Diogo Couto <ldc@eng.au.dk>
2015-10-07 15:55:20 +0200

    Remove no longer used file copy methods.

--- core/src/main/java/org/overture/tools/examplepackager/util/FileUtils.java
+++ core/src/main/java/org/overture/tools/examplepackager/util/FileUtils.java
@@ -28,61 +28,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class FileUtils
 {
-
-	public static List<String> readTextFromJar(String s)
-	{
-		InputStream is = null;
-		BufferedReader br = null;
-		String line;
-		List<String> list = new ArrayList<String>();
-
-		try
-		{
-			is = FileUtils.class.getResourceAsStream(s);
-			br = new BufferedReader(new InputStreamReader(is));
-			while (null != (line = br.readLine()))
-			{
-				list.add(line);
-			}
-		} catch (Exception e)
-		{
-			System.err.println("Failed to read file from jar: \"" + s + "\"");
-			e.printStackTrace();
-		} finally
-		{
-			try
-			{
-				if (br != null)
-				{
-					br.close();
-				}
-				if (is != null)
-				{
-					is.close();
-				}
-			} catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		return list;
-	}
-
-	public static String readFile(String relativePath) throws IOException
-	{
-		StringBuilder sb = new StringBuilder();
-		for (String s : FileUtils.readTextFromJar(relativePath))
-		{
-			sb.append("\n" + s);
-		}
-		return sb.toString();
-	}
 
 	public static boolean detectLatexEnv(InputStream input, String env)
 	{
